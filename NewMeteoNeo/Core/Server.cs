27cab952d772708d190dd24fc080d7fd23a91c9b
xmilicapx <milica.pantelic2001@gmail.com>
2025-06-22 18:30:18 +0200

    Core Class Library, adding Enums, Models and Server class

---
+++ NewMeteoNeo/Core/Server.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Core
+{
+    public static class NetworkHelper
+    {
+        public static void SendMessage(Socket socket, object data)
+        {
+            try
+            {
+                byte[] serializedData;
+                using (var ms = new MemoryStream())
+                {
+                    var bf = new BinaryFormatter();
+                    bf.Serialize(ms, data);
+                    serializedData = ms.ToArray();
+                }
+
+                socket.Send(serializedData);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to send message: {ex.Message}", ex);
+            }
+        }
+
+        public static T DeserializeObject<T>(byte[] data)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                var bf = new BinaryFormatter();
+                return (T)bf.Deserialize(ms);
+            }
+        }
+    }
+}
